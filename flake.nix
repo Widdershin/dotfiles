2026ec38bd2a773f110701d001a35eb339acac40
Nick Johnstone <nick@storypark.nz>
2023-11-02 17:04:49 +1300

    Tenative beginnings of a flake port

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Nick's darwin system";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.05-darwin";
+    nix-darwin.url = "github:LnL7/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs }: {
+    darwinConfigurations."Nicks-MacBook-Pro" = nix-darwin.lib.darwinSystem {
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
